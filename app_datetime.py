c7e2cd92f3503fdf0fbc1a78092d4187c4d84463
Sean Humpherys <shumpherys@wtamu.edu>
2020-10-09 15:31:53 -0500

    ready to code

---
+++ app_datetime.py
@@ -0,0 +1,2 @@
+# Good website for format codes for datetime.strptime()
+# https://www.programiz.com/python-programming/datetime/strptime
---
+++ app_sql.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import json
+import sqlite3
+
+"""
+Watch Mosh's video 9.8. You will need to install
+DB Browser for SQLlite and learn how to create a table
+"""
+
+
+# Chapter 9.8- Working with a SQLite Database
+print("\n9.8- Working with a SQLite Database" + "-" * 20)
+
+# Another way to loop through a list of dictionaries. Important for Chapter 9.8 Databases
+# a json file is a text files formated similar to a python dictionary
